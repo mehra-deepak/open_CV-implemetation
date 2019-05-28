5cbe8280d8cbd53a93e6254696737c6f2552d22d
mehra-deepak <mehradeepak0608@gmail.com>
2019-05-28 22:59:52 +0530

    final

--- rgb_files.py
+++ rgb_files.py
@@ -13,7 +13,9 @@ img[0,2]=(0,0,255) #for red square
 
 img[0,3]=(255) #for white square
 
+img [1,0] = (0,150,150) #dark yellow
 
+# a total of 256 * 256 * 256 =16,777,216 color formation can be made
 
 
 img = cv2.resize(img ,(1000,1000), interpolation =cv2.INTER_AREA)
