4120fa3fc5aece93f680fc5142a74aed3f2ca7b4
mehra-deepak <mehradeepak0608@gmail.com>
2019-05-28 22:55:52 +0530

    understading the RGB colors

---
+++ rgb_files.py
@@ -0,0 +1,23 @@
+import cv2
+
+import numpy as np 
+
+img = np.zeros((10,10,3),np.uint8)
+
+
+img[0,0]=(255 ,0,0) #for blue square 
+
+img[0,1]=(0,255,0) #for green square
+ 
+img[0,2]=(0,0,255) #for red square
+
+img[0,3]=(255) #for white square
+
+
+
+
+img = cv2.resize(img ,(1000,1000), interpolation =cv2.INTER_AREA)
+
+cv2.imshow("IMAGE", img)
+
+cv2.waitKey(0)
