077d062c1d5fe89659b5d6e645c838cecd8cace3
mehra-deepak <mehradeepak0608@gmail.com>
2019-05-28 22:49:06 +0530

    understanding the black and whitw coloring range

--- creating_grids.py
+++ creating_grids.py
@@ -6,6 +6,15 @@ import numpy as np
 
 img = np.zeros((10,10), np .uint8)
 
+img[0,0]=255
+
+img[0,1]=200
+
+img[0,2]=100
+
+img[0,2]=25
+
+
 img = cv2.resize(img ,(1000,1000), interpolation =cv2.INTER_AREA)
 
 cv2.imshow("IMAGE", img)
